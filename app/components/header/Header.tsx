74009051d7dd446c72ff6bfc0e6bdbb1fe4bca6e
Alperen Sirli <alperensirli99@hotmail.com>
2026-02-22 21:35:30 +0100

    basic layout + planning

---
+++ app/components/header/Header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+  return (
+    <h1 className="flex items-center justify-center">Header goes here!</h1>
+  );
+}
---
+++ app/components/sideNav/SideNavBar.tsx
@@ -0,0 +1,15 @@
+export default function SideNavBar() {
+  return (
+    <aside className="w-64 h-full flex flex-col justify-center">
+      <h1>Side Nav goes here!</h1>
+      <nav>
+        <ul>
+          <li>Links goes here</li>
+          <li>Links goes here</li>
+          <li>Links goes here</li>
+          <li>Links goes here</li>
+        </ul>
+      </nav>
+    </aside>
+  );
+}
