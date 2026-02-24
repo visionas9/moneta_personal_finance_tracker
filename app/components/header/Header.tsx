0bcd43090870d0ada10f76d03bfabadb4aee4a88
Alperen Sirli <alperensirli99@hotmail.com>
2026-02-24 21:13:55 +0100

    style: initial layout styling

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,8 +1,16 @@
 export default function Header() {
   return (
-    <div className="flex flex-col items-center justify-center py-4 bg-ink-black text-mint-cream">
-      <h1 className=" font-montserrat text-3xl">Moneta</h1>
-      <p>Your personal finance tracker.</p>
+    <div className="flex justify-between py-4 bg-ink-black text-mint-cream">
+      <div className="flex flex-1 flex-col items-center justify-center ">
+        <h1 className=" font-montserrat font-bold text-4xl">
+          <span className="text-pumpkin-spice">Mon</span>eta
+        </h1>
+        <p className="text-lighter-text">Your personal finance tracker.</p>
+      </div>
+
+      <button className="self-center px-4 py-2 bg-pumpkin-spice mr-10 border-none rounded cursor-pointer hover:bg-pumpkin-dark ">
+        + Add transaction
+      </button>
     </div>
   );
 }
